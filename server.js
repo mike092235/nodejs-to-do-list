b2c160eee11f6d4daf05776854519d91fb5d2e6b
Michael Emmanuel <michael.emmanuel732@gmail.com>
2017-06-18 21:05:54 -0400

    added POST method to form

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ var bodyParser = require('body-parser');
 var app = express();
 var MongoClient = require('mongodb').MongoClient;
 
-  app.listen(3001, function() {
-    console.log('listening on port 3001');
-  });
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(3001, function() {
+  console.log('listening on port 3001');
+});
