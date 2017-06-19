825634f60a34562278eef7136a05bfc886260b81
Michael Emmanuel <michael.emmanuel732@gmail.com>
2017-06-18 21:28:06 -0400

    configured ejs template file

--- server.js
+++ server.js
@@ -5,10 +5,34 @@ var MongoClient = require('mongodb').MongoClient;
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Configure Template Engine
+app.set('view engine', 'ejs');
+
+// Set index.html as default directory
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3001, function() {
+// Connect to Mongo Database
+var db;
+MongoClient.connect('mongodb://mikemike:mikemike@ds127892.mlab.com:27892/my-app-db', (err, database) => {
+  if(err) return console.log(err);
+  db = database;
+  app.listen(3001, function() {
   console.log('listening on port 3001');
+  });
 });
+
+// Create new Collection
+app.post('/quotes', function(req, res) {
+  db.collection('quotes').save(req.body, function(err, results) {
+    if(err) return console.log(err);
+    res.redirect('/');
+  });
+});
+
+// Render form input to "/quotes"
+app.post('/quotes', (req, res) => {
+  console.log(req.body);
+});
+
